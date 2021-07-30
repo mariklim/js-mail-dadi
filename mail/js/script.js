0b2d393480162a46fd6ba6b65f32ca54583966f8
mariklim <maria.klimenko889@gmail.com>
2021-07-30 15:40:26 +0200

    aggiunto base per js, creato lista delle mail

---
+++ mail/js/script.js
@@ -0,0 +1,11 @@
+// 1. Creo la lista delle mail
+// 2. Chiedo all’utente la sua email
+// 3. Controllo che l’utente sia nella lista
+// 4. Stampo un messaggio appropriato sull’esito del controllo
+
+var listMail = ["mail1@gmail.com", "mail2@gmail.com", "mail3@gmail.com"]
+for (var i = 0; i < listMail.length; i++){
+    console.log(listMail[i]);
+}
+
+// var mail = prompt("Inserisci la tua mail");
